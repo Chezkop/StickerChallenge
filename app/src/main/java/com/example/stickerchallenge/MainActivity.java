030e14c166e118248e81102239a014fe800e576b
17001525 <tasker.ewan@gmail.com>
2019-10-04 15:51:01 +0200

    Ewan activity added

--- app/src/main/java/com/example/stickerchallenge/MainActivity.java
+++ app/src/main/java/com/example/stickerchallenge/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.stickerchallenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button ewanGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ewanGo = (Button) findViewById(R.id.ewanGo);
+
+
+        ewanGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this,Ewan.class));
+            }
+        });
     }
 }
