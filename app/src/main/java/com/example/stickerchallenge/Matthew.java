d013e83bffae2b07005677664ee238700e68eeb5
Matthew Barnard <mattbarnard@live.co.za>
2019-10-04 15:56:29 +0200

    Added Matthew Screen

---
+++ app/src/main/java/com/example/stickerchallenge/Matthew.java
@@ -0,0 +1,14 @@
+package com.example.stickerchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Matthew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matthew);
+    }
+}
